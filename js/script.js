4e0bcf1436883a42bf6b75dc9270a967db8fec45
jd-HW <johannes.dereuddre@student.howest.be>
2024-04-20 15:00:44 +0200

    Add ancestry filter

--- js/script.js
+++ js/script.js
@@ -2,17 +2,19 @@ import { potterCharacters } from "./datafile.js";
 
 "use strict";
 
-let homes, filteredByHouse;
+let homes, filteredByHouse, houseList, mainEl;
 
 window.addEventListener("load", initialise);
 
 function initialise() {
-    
+
+    houseList = document.getElementById("houses");
+    mainEl = document.getElementById("main");
     console.log(potterCharacters);
     getAllHomes();
     makeSelectOptions();
 
-    document.addEventListener("input", filterHouseBasedOnSelection)
+    houseList.addEventListener("input", filterHouseBasedOnSelection)
 
 }
 
@@ -34,7 +36,6 @@ function getAllHomes(){
 
 function makeSelectOptions(){
 
-    const houseList = document.getElementById("houses");
     houseList.options[houseList.length] = new Option("All", "All");
 
     homes.forEach(home => {
@@ -63,7 +64,7 @@ function filterByHouse(residence){
 
 function populateSelection(residence){
 
-    const mainEl = document.getElementById("main");
+    // const mainEl = document.getElementById("main");
 
     mainEl.innerHTML = "";
 
@@ -98,7 +99,8 @@ function populateSelection(residence){
 
 }
 
-function makeRadioButtons(selection){
+
+function makeRadioButtons(){
 
     const divEl = document.getElementById("ancestry");
     divEl.innerHTML = "";
@@ -116,15 +118,15 @@ function makeRadioButtons(selection){
         }
     });    
 
-
     console.log(ancestryOptions);
 
     const radioAllEl = document.createElement("input");
     const radioLabelForAll = document.createElement("label");
     radioAllEl.type = "radio";
     radioAllEl.id = "All";
-    radioAllEl.name = "All";
+    radioAllEl.name = "ancestry";
     radioAllEl.value = "All";
+    radioAllEl.addEventListener("click", filterByAncestry)
     radioLabelForAll.for = "All";
     radioLabelForAll.innerText = "All";
     divEl.append(radioAllEl);
@@ -136,11 +138,50 @@ function makeRadioButtons(selection){
         const radioLabel = document.createElement("label");
         radioEl.type = "radio";
         radioEl.id = item;
-        radioEl.name = item;
+        radioEl.name = "ancestry";
         radioEl.value = item;
+        radioEl.addEventListener("click", filterByAncestry)
         radioLabel.for = item;
         radioLabel.innerText = item;
         divEl.append(radioEl);
         divEl.append(radioLabel);
     })
+}
+
+function filterByAncestry(e){
+
+    let result = filteredByHouse.filter(person => person.ancestry === e.target.value);
+
+    if (e.target.value ==="All"){
+        result = filteredByHouse;
+    }
+
+    mainEl.innerHTML = "";
+
+    result.forEach(person => {
+        
+        const articleEl = document.createElement("article");
+
+        if(person.house === ""){
+            articleEl.classList.add("no-data");
+        }
+        else{
+            articleEl.classList.add(person.house.toLowerCase());
+        }
+
+        const nameEl = document.createElement("h1");
+        const imageEl = document.createElement("img");
+        imageEl.src = person.image;
+        const actorNameEl = document.createElement("p");
+
+        nameEl.innerText = person.name;
+        actorNameEl.innerText = person.actor;
+
+        articleEl.append(nameEl);
+        articleEl.append(imageEl);
+        articleEl.append(actorNameEl);
+        mainEl.append(articleEl);
+
+    });
+   
 }
