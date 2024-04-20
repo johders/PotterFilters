4c384b8d412dd23c5f16128b233bc18ae703d1d4
jd-HW <johannes.dereuddre@student.howest.be>
2024-04-20 12:36:03 +0200

    Import data from character data file

--- js/datafile.js
+++ js/datafile.js
@@ -1,4 +1,4 @@
-const  potterCharacters =
+export const  potterCharacters =
 [{
     id: "9e3f7ce4-b9a7-4244-b709-dae5c1f1d4a8",
     name: "Harry Potter",
--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
+import { potterCharacters } from "./datafile.js";
+
 "use strict";
 
 window.addEventListener("load", initialise);
 
 function initialise() {
-
+    
+    console.log(potterCharacters);
+    
 }
 
